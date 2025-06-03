163fc2011560c20e577355a20817bf3625f5e999
nicollasprado <nicollasprado.profissional@gmail.com>
2025-06-03 18:59:51 -0300

    refactor: added min name length and removed non-useful parts

--- src/server/controllers/city/CreateCity.ts
+++ src/server/controllers/city/CreateCity.ts
@@ -1,25 +1,20 @@
-import { Request, RequestHandler, Response } from "express";
+import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { z } from "zod";
 import { validation } from "../../shared/middlewares";
 
 const bodySchema = z.object({
-  name: z.string(),
-});
-
-const querySchema = z.object({
-  filter: z.string().optional(),
+  name: z.string().min(3),
 });
 
 type TCity = z.infer<typeof bodySchema>;
 
 export const createValidation = validation({
   body: bodySchema,
-  query: querySchema,
 });
 
 export const create = (req: Request<{}, {}, TCity>, res: Response) => {
-  const data = bodySchema.parse(req.body);
+  const data = req.body;
 
   res.status(StatusCodes.CREATED).send("Criado");
 };
